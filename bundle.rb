db6fa4bd9e0516aa617abf40518bb7a152e19b11
sgtcoolguy <cwilliams@aptana.com>
2010-06-14 13:15:08 -0400

    add description and repository

--- bundle.rb
+++ bundle.rb
@@ -2,6 +2,8 @@ require 'ruble'
 
 bundle do |bundle|
   bundle.display_name = "Zen Coding"
+  bundle.description = "Port of Zen Coding for Studio 3"
+  bundle.repository = "git://github.com/aptana/zen-coding.ruble.git"
 
   bundle.menu 'Zen Coding' do |main_menu|
     main_menu.command 'Expand Abbreviation'
