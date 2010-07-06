945c93ad43555e9d138b1d8f131e6f81d35b114b
unknown <Christopher Williams@.(none)>
2010-07-06 12:30:30 -0400

    get command working on windows 7 (for me)

--- commands/expand_abbreviation.rb
+++ commands/expand_abbreviation.rb
@@ -5,5 +5,14 @@ command 'Expand Abbreviation' do |cmd|
   cmd.output = :insert_as_snippet
   #cmd.scope = :all
   cmd.key_binding = 'M1+E'
-  cmd.invoke = 'python "$TM_BUNDLE_SUPPORT"/expand_abbreviation.py'
+  cmd.invoke do |context|
+    ipath = com.aptana.core.util.ExecutableUtil.find("python", true, [org.eclipse.core.runtime.Path.fromOSString("C:/Python27")])
+    python_exe = ipath.nil? ? 'python' : ipath.toOSString
+    result = IO.popen("#{python_exe} \"#{ENV['TM_BUNDLE_SUPPORT']}/expand_abbreviation.py\"", "r+") do |io|
+      io.write $stdin.read
+      io.close_write # let the process know you've given it all the data 
+      io.read
+    end
+    result
+  end
 end
