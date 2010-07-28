c0e7d548e7b533c6daa7c5d6a5787c78ad6fe240
sgtcoolguy <cwilliams@aptana.com>
2010-07-28 12:34:22 -0400

    more work on RR3 #700 - Zed Coding does not work on Windows OS
    try to give users better errors if command fails. Handle when python not installed/found on PATH (tested on Mac)

--- commands/expand_abbreviation.rb
+++ commands/expand_abbreviation.rb
@@ -6,14 +6,20 @@ command 'Expand Abbreviation' do |cmd|
   #cmd.scope = :all
   cmd.key_binding = 'M1+E'
   cmd.invoke do |context|
-    ipath = com.aptana.core.util.ExecutableUtil.find("python", true, [org.eclipse.core.runtime.Path.fromOSString("C:/Python27"), org.eclipse.core.runtime.Path.fromOSString("C:/Python26")])
-    python_exe = ipath.nil? ? 'python' : ipath.toOSString
-    result = IO.popen("#{python_exe} \"#{ENV['TM_BUNDLE_SUPPORT']}/expand_abbreviation.py\"", "r+") do |io|
-      io.write $stdin.read
-      io.close_write # let the process know you've given it all the data 
-      io.read
+    begin
+      ipath = com.aptana.core.util.ExecutableUtil.find("python", true, [org.eclipse.core.runtime.Path.fromOSString("C:/Python27"), org.eclipse.core.runtime.Path.fromOSString("C:/Python26")])
+      python_exe = ipath.nil? ? 'python' : ipath.toOSString
+      result = IO.popen("#{python_exe} \"#{ENV['TM_BUNDLE_SUPPORT']}/expand_abbreviation.py\"", "r+") do |io|
+        io.write $stdin.read
+        io.close_write # let the process know you've given it all the data 
+        io.read
+      end
+      context.exit_discard if (result.nil? or result.strip.length == 0)
+      result
+    rescue Errno::EPIPE
+      context.exit_show_tooltip "Unable to find 'python' executable. Please be sure it is installed and on the PATH"
+    rescue => e
+      context.exit_show_tooltip e.to_s
     end
-    context.exit_discard if (result.nil? or result.strip.length == 0)
-    result
   end
 end
